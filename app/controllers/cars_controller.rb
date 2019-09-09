a75c635492838e6381ee2a88aa44c1114721a698
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-09-09 18:33:33 -0500

    created routes for inventory and about

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,13 @@
 class CarsController < ApplicationController
   def index
-    
+
+  end
+
+  def inventory
+
+  end
+
+  def about
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'cars#index'
-  get "car" => "cars#index"
+  get "car", to: "cars#index"
+  get "inventory", to: "cars#inventory"
+  get "about", to: "cars#about"
 end
