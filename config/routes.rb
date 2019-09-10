79b32f91e0fda3182c89308f58a96923376b2c05
Trey Westrich <treyw05@gmail.com>
2019-09-09 19:30:56 -0500

    routes for contact and about

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'cars#index'
   get "car", to: "cars#index"
   get "inventory", to: "cars#inventory"
-  get "about", to: "cars#about"
+  get "about", to: "infopages#about"
+  get "contact", to: "infopages#contact"
 end
