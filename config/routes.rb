72057a6ad1ed693e8babd3628d85328c356dbbc5
NeonDactyl <treyw05@gmail.com>
2019-09-12 19:25:03 -0500

    fixed link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'vehicles#index'
-  get "car", to: "vehicles#index"
-  get "inventory", to: "vehicles#inventory"
+  resources :vehicles
   get "about", to: "info_pages#about"
   get "contact", to: "info_pages#contact"
 end
