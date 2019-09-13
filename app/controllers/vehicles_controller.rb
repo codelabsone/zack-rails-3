8a664dbb03f487371911a2fcb4b57de4a246e06f
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-09-12 19:51:55 -0500

    added create method and defined behicle params

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -10,4 +10,15 @@ class VehiclesController < ApplicationController
   def edit
     @vehicle = Vehicle.find(params[:id])
   end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:year, :make, :model, :price, :new, :mileage, :transmission, :engine, :drivetrain, :vin, :fuel_type, :body_style, :ext_color, :int_color, :mpg)
+  end
+  
 end
