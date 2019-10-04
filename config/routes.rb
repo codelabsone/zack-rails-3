ed97e2652418e9d06b7d0d82a87366ac1f918fae
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-10-03 19:40:29 -0500

    added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
+  resource :session
+  get "signout", to: "sessions#destroy"
+
   resources :users
+
   root 'vehicles#index'
   resources :vehicles do
     member do
       delete :delete_image_attachment
     end
   end
+
   get "about", to: "info_pages#about"
   get "contact", to: "info_pages#contact"
+
 end
