b343fd88392e855358dba8411ff2faaedca5001c
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-10-03 19:18:55 -0500

    adding form and method for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+private
+
+  def current_user
+    @current_user ||= User.find(session[:id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user
+  helper_method :current_user?
+
 end
